71048809d4c0d70a8118150d9922d13bb86bd02f
david griffin <65681542+davidleegriffin@users.noreply.github.com>
2021-05-06 17:17:37 -0500

    Delete aws-exports.js

--- aws-exports.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    identityPoolId: 'us-east-2:417780a8-d4fe-40ac-b6d0-6e91ae3721de',
-    region: 'Regions.US_EAST_2',
-    userPoolId: 'us-east-2_Mzj8R0lRU',
-    userPoolWebClientId: '78hc501hf4hhgiamfk4l0eecq4'
-}
-
-export default config;
